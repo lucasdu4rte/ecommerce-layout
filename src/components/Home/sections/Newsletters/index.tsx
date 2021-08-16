4e94b745e2c4610e8628b5b5f47f34fe5d2509ba
Lucas Duarte <lucasdu4rte@gmail.com>
2021-08-15 23:43:12 -0300

    feature: submit data to api

--- src/components/Home/sections/Newsletters/index.tsx
+++ src/components/Home/sections/Newsletters/index.tsx
@@ -1,27 +1,48 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 import styles from "./styles.module.scss";
 
+type FormData = {
+  name: string;
+  email: string;
+};
+
 const Newsletters = () => {
+  const [isServerError, setServerError] = useState(false);
   const {
     register,
     handleSubmit,
     reset,
-    formState: { errors, isSubmitSuccessful },
+    formState: { errors, isSubmitting, isSubmitSuccessful },
   } = useForm();
 
-  const onSubmit = (data: any) => console.log(data);
+  const onSubmit = async (data: FormData) => {
+    try {
+      setServerError(false);
+      await axios.post(
+        "https://corebiz-test.herokuapp.com/api/v1/newsletter",
+        data
+      );
+    } catch (err) {
+      alert("Houve um problema, tente novamente por favor");
+      setServerError(true);
+    }
+  };
 
   return (
     <section>
       <div className={styles.container}>
-        {isSubmitSuccessful ? (
+        {!isSubmitting && isSubmitSuccessful && !isServerError ? (
           <div className={styles.successMessage}>
             <strong>Seu e-mail foi cadastrado com sucesso!</strong>
             <p>
               A partir de agora você receberá as novidade e ofertas exclusivas.
             </p>
-            <button type="button" onClick={() => reset()}>Cadastrar novo e-mail</button>
+            <button type="button" onClick={() => reset()}>
+              Cadastrar novo e-mail
+            </button>
           </div>
         ) : (
           <form onSubmit={handleSubmit(onSubmit)}>
@@ -51,7 +72,9 @@ const Newsletters = () => {
                 />
                 {errors.email && <span>Preencha com um e-mail válido</span>}
               </label>
-              <button type="submit">Eu quero!</button>
+              <button type="submit" disabled={isSubmitting}>
+                {isSubmitting ? "Enviando..." : "Eu quero!"}
+              </button>
             </div>
           </form>
         )}
